99632725de29ea1dcc9e7027b5fd33674bc5cf98
Beyzatass <beyzavtas@gmail.com>
2020-10-27 14:25:22 +0300

    feat:MVCCore added

---
+++ WebUI/Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abc.Core.Utilities.Results
+{
+    public class SuccessResult: Result
+    {
+        public SuccessResult(string message) : base(true, message)
+        { 
+        }
+        public SuccessResult() : base(true)
+        { 
+        }
+    }
+}
