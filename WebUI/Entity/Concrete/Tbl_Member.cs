a1645e606dacea196b341c30813a0b3433c6dc12
AynurGunn <aynur.gunn@hotmail.com>
2020-10-27 14:59:16 +0300

    feat:MVCEntity added

---
+++ WebUI/Entity/Concrete/Tbl_Member.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity.Concrete
+{
+   public class Tbl_Member:IEntity
+    {
+        [Key]
+        public int MemberID { get; set; }
+        public int IDNumber { get; set; }
+        public string MemberName { get; set; }
+        public string MemberSurname { get; set; }
+        public string MemberMail { get; set; }
+        public int MemberPassword { get; set; }
+       
+    }
+}
---
+++ WebUI/Entity/Concrete/Tbl_Request.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity.Concrete
+{
+   public class Tbl_Request:IEntity
+    {
+        [Key]
+        public int RequestID { get; set; }
+        public int MemberID { get; set; }
+        public int AdID { get; set; }
+        public int RequestStateID { get; set; }
+        
+    }
+}
